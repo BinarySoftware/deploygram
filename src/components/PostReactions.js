2ec2a868405ac4e426d721071d5dd3a41d3cb9e6
BinarySoftware <mkmikolajek@icloud.com>
2021-11-27 15:14:52 +0100

    save

--- src/components/PostReactions.js
+++ src/components/PostReactions.js
@@ -2,18 +2,40 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
+import React from 'react';
+import axios from 'axios';
 
-const PostReactions = () => {
+const PostReactions = (postId) => {
+    const [isLiked, setIsLiked] = React.useState(false);
+
+    React.useEffect( () => {
+        setLikes();
+    });
+
+    const setLikes = async () => {
+        await axios.get('https://deploygram.deployed.space/posts/' + postId + '/likes');
+    };
+
+    const toggleLike = () => {
+        setIsLiked(!isLiked);
+    };
+
     const pStyle = {
         margin: 0,
         marginLeft: '5px'
     };
 
+    const icon = () => {
+        if (isLiked) {
+            return (<FavoriteIcon />);
+        }
+        return (<FavoriteBorderIcon />);
+    };
+
     return (
         <Box>
             <Stack direction="row" width="100%" marginX="auto">
-                <FavoriteBorderIcon />
-                <FavoriteIcon />
+                <button onClick={ toggleLike }>{ icon() }</button>
                 <b><p style={pStyle}>9 likes</p></b>
             </Stack>
         </Box>
